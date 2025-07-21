84efd50e3ec9523c504ff375dd6fa8a7cf003a8d
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2025-07-21 18:12:38 +0530

    Update menuitems.js

--- routes/menuitems.js
+++ routes/menuitems.js
@@ -6,18 +6,4 @@ const router = require("express").Router();
 
 
 
-
-// Get a specific menu item by ID
-router.route("/get/:id").get(async (req, res) =>{
-    let MenuId = req.params.id;
-    const Menu = await MenuItem.findById(MenuId)
-    .then((menuitem) => {
-        res.status(200).send({status: "User fetched",menuitem})
-    }).catch(() => {
-        console.log(err.message);
-        res.status(500).send({status: "error with", error: err.message})
-    })
-})
-
-
 module.exports = router;
