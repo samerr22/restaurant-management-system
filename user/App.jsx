d24bc065c7c7c547a8746763e86507bcd023e392
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2025-08-01 21:24:51 +0530

    Update App.jsx

--- user/App.jsx
+++ user/App.jsx
@@ -3,12 +3,7 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Homed from './Homed';
-import Table from './Table';
-import Order from './Order';
-import Payment from './Payment';
-import Umenu from './admin/Umenu';
-import Utable from './admin/Utable';
-import Upayment from './admin/Upayment'
+
 
 
 
