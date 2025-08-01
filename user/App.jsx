054046b8ce18963122b68ca68432dd0b77b09c9e
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2025-08-01 21:25:52 +0530

    Update App.jsx

--- user/App.jsx
+++ user/App.jsx
@@ -1,8 +1,6 @@
 
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Homed from './Homed';
 
 
 
