061650407b36b22c8bf7b1c55905091e6d937b75
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2025-09-23 07:00:46 +0530

    Update paymet.js

--- routes/paymet.js
+++ routes/paymet.js
@@ -2,23 +2,12 @@ const router = require("express").Router();
 let Paymet = require("../models/Paymet"); 
 
 
-// creat a new menu
 
-
-
-  
-
-
-//http//localhost:8070/menuitem/delete/45dfldfldfg45fg
-// Delete a menu item by ID
-
-
-
-})
 
 
 
 module.exports = router;
+
 
 
 
